4714aca2fe6577906c91b1fa6ffd42e1acbce5ea
Augie Schwer <augie@safaribooksonline.com>
2014-04-25 17:37:45 -0700

    Added some more grammar from the syslog.lua

--- sandbox/lua/decoders/nagios.lua
+++ sandbox/lua/decoders/nagios.lua
@@ -4,13 +4,19 @@ Parses the Nagios log file
 
 Here's the grammar I have so far that's tested with http://lpeg.trink.com/
 
-Input: "[1398461360] SERVICE ALERT:"
-Result: "1398461360"
+Input: "[1398461360] SERVICE ALERT: example.com"
+Result: "1398461360" , "example.com"
 
 local l = require 'lpeg'
 l.locale(l)
-unix_date = l.digit^1
-grammar = "[" * l.C( unix_date ) * "] SERVICE ALERT:"
+
+local nilvalue        = l.P"-"
+local printusascii    = l.R"!~"
+
+local unix_date = l.digit^1
+local hostname = nilvalue + printusascii^-255
+
+grammar = "[" * l.C( unix_date ) * "] SERVICE ALERT: " * l.C( hostname )
 
 --]]
 
