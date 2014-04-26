fad3cd70d94339418640aeb35c535e516ebda4bd
Augie Schwer <augie@safaribooksonline.com>
2014-04-25 17:20:34 -0700

    Add the small bit of grammar I have working.

--- sandbox/lua/decoders/nagios.lua
+++ sandbox/lua/decoders/nagios.lua
@@ -2,6 +2,16 @@
 
 Parses the Nagios log file
 
+Here's the grammar I have so far that's tested with http://lpeg.trink.com/
+
+Input: "[1398461360] SERVICE ALERT:"
+Result: "1398461360"
+
+local l = require 'lpeg'
+l.locale(l)
+unix_date = l.digit^1
+grammar = "[" * l.C( unix_date ) * "] SERVICE ALERT:"
+
 --]]
 
 local template = read_config("template")
